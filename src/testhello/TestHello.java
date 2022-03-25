08b54af546b50a85ad897f4c3ec29100cb305661
suchakreehub <suchakree.kg@gmail.com>
2022-03-25 14:39:33 +0700

    third commit

--- src/testhello/TestHello.java
+++ src/testhello/TestHello.java
@@ -15,7 +15,8 @@ public class TestHello {
      */
     public static void main(String[] args) {
         System.out.println("Hello World.");
-        System.out.println("Welcome");
+        System.out.println("Welcome to Thailand");
+        System.out.println("Have a Good Trip");
     }
     
 }
